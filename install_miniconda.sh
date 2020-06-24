e0973e594330971acacd101bd82bb851e2edf5a5
Jiangwei Yao <yao.jiangwei@gmail.com>
2020-06-24 15:09:23 -0400

    conda channel fix

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -6,6 +6,9 @@ echo 'export PATH="~/miniconda3/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
 conda init bash
 conda config --set auto_activate_base false
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
 . ~/.bashrc
 rm Miniconda3-latest-Linux-x86_64.sh
 
